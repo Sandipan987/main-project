c92e0d6785abbc8a50d7051cafd11c55f559ed25
Sandipan <sandipanthombre555@gmail.com>
2024-06-18 15:51:42 +0530

    "commit for service layer api done"

--- demo/demo/src/main/java/com/example/demo/service/UserServiceI.java
+++ demo/demo/src/main/java/com/example/demo/service/UserServiceI.java
@@ -4,6 +4,11 @@ import com.example.demo.model.User;
 
 import java.util.List;
 
-public interface UserServiceI {
 
+public interface UserServiceI {
+    User createUser(User user);
+    User updateUser(User user,Long userId);
+    User getSingleUser(Long userId);
+    List<User> getAllUsers();
+    void deleteUser(Long userId);
 }
--- demo/demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ demo/demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -9,4 +9,52 @@ import java.util.List;
 import java.util.Optional;
 
 public class UserServiceImpl implements UserServiceI {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User createUser(User user) {
+        User saveUser=userRepository.save(user);
+
+        return saveUser;
+    }
+
+    @Override
+    public User updateUser(User user, Long userId) {
+        User user1=userRepository.findById(userId).get();
+        user1.setUserName(user.getUserName());
+        user1.setUserAge(user.getUserAge());
+        user1.setAbout(user.getAbout());
+        User updateUser=userRepository.save(user1);
+        return updateUser;
+    }
+
+    @Override
+    public User getSingleUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resource not found on server" + userId));
+        return user;
+     /*
+       Optional<User>user= userRepository.findById(userId);
+       if (user.isPresent()){
+           return user.get();
+       }else{
+           throw new NullPointerException("Resource not found on server" + userId);
+       }
+      */
+
+
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> all = userRepository.findAll();
+        return all;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resource not found on server" + userId));
+        userRepository.delete(user);
+    }
 }
