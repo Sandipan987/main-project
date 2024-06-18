9c4d327289234d2557121cffbb568be24cb566b4
Sandipan <sandipanthombre555@gmail.com>
2024-06-18 15:24:18 +0530

    first commit to make project structure

---
+++ demo/demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserServiceI {
+}
