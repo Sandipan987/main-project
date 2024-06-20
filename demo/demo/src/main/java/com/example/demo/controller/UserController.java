c0d25dfa78ebe8e38957d6f260e1d67605ecd6a5
Sandipan <sandipanthombre555@gmail.com>
2024-06-20 15:48:47 +0530

    "implemented logger for service and controller layer"

--- demo/demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserServiceI;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,33 +13,52 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class UserController {
     @Autowired
 private UserServiceI userServiceI;
 
     @PostMapping("/user")
     public ResponseEntity<User> createUser(@RequestBody User user){
+   log.info("Initiating request to create user");
         User user1 = userServiceI.createUser(user);
+        log.info("Complete request to create user");
+
         return new ResponseEntity<>(user1, HttpStatus.CREATED);
 }
 @GetMapping
 public ResponseEntity<List<User>>getAllUser(){
+    log.info("Initiating request to get All user");
     List<User> allUsers = userServiceI.getAllUsers();
+    log.info("Complete request to get All user");
+
     return new ResponseEntity<>(allUsers,HttpStatus.OK);
 }
 @GetMapping("/users/{userId}")
 public ResponseEntity<User> getSingleUser(@PathVariable Long userId) {
+    log.info("Initiating request to get Single user");
+
     User user = userServiceI.getSingleUser(userId);
+    log.info("Complete request to get Single user");
+
     return new ResponseEntity<>(user, HttpStatus.OK);
 }
 @PutMapping("/users/{userId}")
 public ResponseEntity<User>updateUser(@RequestBody User user,@PathVariable Long userId){
+    log.info("Initiating request to update user");
+
     User user1 = userServiceI.updateUser(user,userId);
+    log.info("Complete request to update user");
+
     return new ResponseEntity<>(user1, HttpStatus.CREATED);
 
 }
 public ResponseEntity<String>deleteUser(@PathVariable Long userId){
+    log.info("Initiating request to delete user");
+
     userServiceI.deleteUser(userId);
+    log.info("Complete  request to delete user");
+
     return new ResponseEntity<>("Resource Delete Successful !!",HttpStatus.OK);
 
 }
--- demo/demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ demo/demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,36 +3,49 @@ package com.example.demo.service.impl;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserServiceI;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 @Service
+@Slf4j
 public class UserServiceImpl implements UserServiceI {
 
     @Autowired
     private UserRepository userRepository;
     @Override
     public User createUser(User user) {
+        log.info("Initiating Service to create user");
+
         User saveUser=userRepository.save(user);
+        log.info("Complete Service to create user");
 
         return saveUser;
     }
 
     @Override
     public User updateUser(User user, Long userId) {
+        log.info("Initiating Service to update user");
+
         User user1=userRepository.findById(userId).get();
         user1.setUserName(user.getUserName());
         user1.setUserAge(user.getUserAge());
         user1.setAbout(user.getAbout());
         User updateUser=userRepository.save(user1);
+        log.info("complete Service to update user");
+
         return updateUser;
     }
 
     @Override
     public User getSingleUser(Long userId) {
+        log.info("Initiating Service to get single user");
+
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resource not found on server" + userId));
+        log.info("Complete  Service to get user");
+
         return user;
      /*
        Optional<User>user= userRepository.findById(userId);
@@ -49,13 +62,21 @@ public class UserServiceImpl implements UserServiceI {
 
     @Override
     public List<User> getAllUsers() {
+        log.info("Initiating Service to get All user");
+
         List<User> all = userRepository.findAll();
+        log.info("Complete Service to get All user");
+
         return all;
     }
 
     @Override
     public void deleteUser(Long userId) {
+        log.info("Initiating  Service to a  user");
+
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("Resource not found on server" + userId));
         userRepository.delete(user);
+        log.info("Complete Service to delete user");
+
     }
 }
